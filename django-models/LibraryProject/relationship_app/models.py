1ed7da7bb657b4cec8c218bd6c948c121693d512
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-11-08 23:05:39 +0200

    new commit

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Librarian(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    employee_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    published_date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    librarian = models.OneToOneField(Librarian, on_delete=models.CASCADE, null=True, blank=True)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,38 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def get_books_by_author(author_name):
+    try:
+        author  = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        print(f"\n📚 Books written by {author_name}: ")
+        for book in books:
+            print(f"- {book.title}")
+    except Author.DoesNotExist:
+        print(f"\n ❌ Author '{author_name}' not found my guy.")
+
+
+def list_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        print(f"\n🏛️ Books available in {library_name}: ")  
+        for book in books:
+            print(f"- {book.title}")
+    except Library.DoesNotExist:
+        print(f"\n ❌ Library '{Library_name}' not found.")  
+
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian
+        if librarian:
+            print(f"\n👲 librarian for {librarian.name}: {librarian.user.username}")
+        else:
+            print(f"\n💀{library.name} does not have a library assigned. ")
+    except Library.DoesNotExist:
+        print(f"\n ❌ Library '{library_name}' not found.")
+
+if __name__ == "__main__":
+    get_books_by_author("John Doe")
+    list_books_in_library("City library")
+    get_librarian_for_library("City Library")
