668ec50a113d738faae0e4d433a49d4a867236f9
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-11-09 17:08:18 +0200

    new commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ class SignUpView(CreateView):
     template_name = "registration/signup.html"
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
