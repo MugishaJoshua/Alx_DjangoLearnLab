734a5962244113f9a4b9c41ed230b9811e1e423f
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-11-13 11:25:56 +0200

    new commit

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/accounts/profile"
 LOGOUT_REDIRECT_URL = "/accounts/profile"
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = "bookshelf.CustomUser"
