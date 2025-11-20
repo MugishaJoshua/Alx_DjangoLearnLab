f7c4e8d2104ef9929e314a3e43168e5859f434cd
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-11-21 00:37:03 +0200

    new commit

---
+++ api_project/api/models.py
@@ -0,0 +1,8 @@
+from django.db import  models
+
+class book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
