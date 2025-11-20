85efc160dff9eb37d968d005e931cd6bb38b3daf
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-11-21 01:38:11 +0200

    new commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# Simple list view (optional)
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Full CRUD viewset
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
