8e4b5fd5ee88e22451bb5464b0399f657f391922
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-11-21 02:12:59 +0200

    new commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
+from .views import BookList
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookViewSet
 
-# Router for BookViewSet
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('auth-token/', obtain_auth_token, name='api_token_auth'),  # <- token endpoint
-    path('', include(router.urls)),  # existing router
+    # Include the old BookList view
+    path('books/', BookList.as_view(), name='book-list'),
+
+    # Include the router for BookViewSet
+    path('', include(router.urls)),
 ]
