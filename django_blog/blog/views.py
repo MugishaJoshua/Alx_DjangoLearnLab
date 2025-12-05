652c15f2550e4691a9dc1367b937ee0e4f195867
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-12-05 23:53:48 +0200

    new commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm,  ProfileUpdateForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log user in immediately
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'blog/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})
