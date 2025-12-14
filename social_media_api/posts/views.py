bb4376a25ead3761aa5d6e1a0969e3abd494795c
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-12-14 22:48:30 +0200

    new commit

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
-from .models import Post
 from .serializers import PostSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.contenttypes.models import ContentType
+from .models import Post, Like
+from notifications.models import Notification
+
 
 
 class FeedView(APIView):
@@ -15,3 +21,37 @@ class FeedView(APIView):
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+class LikePostView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+
+        like, created = Like.objects.get_or_create(
+            user=request.user,
+            post=post
+        )
+
+        if not created:
+            return Response({"detail": "Post already liked"}, status=400)
+
+        if post.author != request.user:
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                content_type=ContentType.objects.get_for_model(post),
+                object_id=post.id
+            )
+
+        return Response({"detail": "Post liked"})
+
+
+class UnlikePostView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        Like.objects.filter(user=request.user, post=post).delete()
+        return Response({"detail": "Post unliked"})
