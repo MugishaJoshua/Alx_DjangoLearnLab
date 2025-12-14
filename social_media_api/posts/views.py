f75da15031795d97e01df703cee504c35386308f
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-12-14 22:33:53 +0200

    new commit

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,19 +1,17 @@
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
 
 class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         following_users = request.user.following.all()
 
-        posts = Post.objects.filter(
-            author__in=following_users
-        ).order_by('-created_at')
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
