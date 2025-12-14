a63e4bba774418925debe586e0309428464c773f
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-12-14 22:07:22 +0200

    new commit

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,6 +6,9 @@ from .serializers import (
     LoginSerializer,
     UserProfileSerializer
 )
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -27,3 +30,36 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+User = get_user_model()
+
+
+class FollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(User, id=user_id)
+
+        if user_to_follow == request.user:
+            return Response(
+                {"detail": "You cannot follow yourself."},
+                status=400
+            )
+
+        request.user.following.add(user_to_follow)
+        return Response(
+            {"detail": f"You are now following {user_to_follow.username}."}
+        )
+
+
+class UnfollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(User, id=user_id)
+
+        request.user.following.remove(user_to_unfollow)
+        return Response(
+            {"detail": f"You unfollowed {user_to_unfollow.username}."}
+        )
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,6 +3,10 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -31,3 +35,18 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        followed_users = request.user.following.all()
+
+        posts = Post.objects.filter(
+            author__in=followed_users
+        ).order_by('-created_at')
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
