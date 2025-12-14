17b9a50425cea57cd018a7c2632b9a7af4e353be
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-12-14 21:08:34 +0200

    new commit

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        Token.objects.create(user=user)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return {
+            'user_id': user.id,
+            'username': user.username,
+            'token': token.key
+        }
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
+    following_count = serializers.IntegerField(source='following.count', read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'bio',
+            'profile_picture',
+            'followers_count',
+            'following_count'
+        ]
